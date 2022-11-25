c89deaa342e06d33f412c523ea6e079199ca4930
Benardo E. Steinmacher <bsteinmacher@outlook.com>
2022-11-25 18:49:33 -0300

    Exercicio 4/9

--- 01_fundamentos/04_Introducao-a-JavaScript/Dia02-JavaScript-Array-loop-For/javascript.js
+++ 01_fundamentos/04_Introducao-a-JavaScript/Dia02-JavaScript-Array-loop-For/javascript.js
@@ -16,3 +16,13 @@ console.log("O valor total é: " + soma);
 // A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
 let media = soma / numbers.length;
 console.log('Média aritimética é: ' + media);
+
+// Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+if (media > 20) {
+    console.log('Valor maior que 20!');
+}
+    else {
+        console.log('Valor é menor que 20!');
+    
+}
+
